5661d707a5cc226a81b26648e5ec5186b792b4ed
DangThangHalowwin09 <thangdang@panthera.vn>
2025-04-11 01:25:49 +0700

    cmt

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,19 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_to_do_list/View/login_screen.dart';
 import 'package:flutter_to_do_list/firebase_options.dart';
+import 'package:flutter_portal/flutter_portal.dart'; //
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+
+  runApp(
+    Portal( // 👈 bọc Portal ở đây
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
