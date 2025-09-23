f1406d469a22fe852e78f986015950c11c1b7e0c
ThangDQ <107424970+DangThangHalowwin09@users.noreply.github.com>
2025-09-23 16:36:13 +0700

    Change code firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,25 @@ import 'package:flutter/material.dart';
 import 'package:flutter_to_do_list/View/login_screen.dart';
 import 'package:flutter_to_do_list/firebase_options.dart';
 import 'package:flutter_portal/flutter_portal.dart';
+import 'package:flutter_to_do_list/services/firebase_messaging_service.dart';
+import 'package:flutter_to_do_list/services/local_notifications_service.dart';
 import 'View/roledirection_screen.dart';
 import 'firebase_msg.dart'; //
 
+void push_notification_setup() async{
+  final localNotificationsService = LocalNotificationsService.instance();
+  await localNotificationsService.init();
+
+  final firebaseMessagingService = FirebaseMessagingService.instance();
+  await firebaseMessagingService.init(localNotificationsService: localNotificationsService);
+}
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  await initFCM();
+  push_notification_setup();
+  //await initFCM();
 
   runApp(
     Portal( // 👈 bọc Portal ở đây
